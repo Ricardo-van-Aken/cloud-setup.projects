706e902ee8b3eabceb47c47ff05cc6abdf5c6434
Zeepaardje <rvaken98@gmail.com>
2025-10-30 18:25:20 +0100

    yeet

--- scripts/common.sh
+++ scripts/common.sh
@@ -30,7 +30,7 @@ skip_metadata_api_check     = true
 skip_region_validation      = true
 skip_s3_checksum            = true
 
-# Enable lockfile-based state locking in Spaces (Terraform >= 1.11)
+# Enable lockfile-based state locking in Spaces
 use_lockfile = true
 EOF
 
