8586244c5ad31e75d546667f7a1cf899ddfb7dec
Ricardo van Aken <r.v.aken@protonmail.com>
2026-06-02 20:26:15 +0200

    removed pro feature (since we dont have pro)

--- projects/pi-VPN/main.tf
+++ projects/pi-VPN/main.tf
@@ -46,26 +46,9 @@ resource "github_team_repository" "development_brie" {
   permission = "push"
 }
 
-resource "github_branch_protection" "main" {
-  repository_id = github_repository.this.name
-  pattern       = "main"
-
-  required_pull_request_reviews {
-    required_approving_review_count = 1
-    dismiss_stale_reviews           = true
-    require_code_owner_reviews      = true
-  }
-
-  required_status_checks {
-    strict = true
-  }
-
-  enforce_admins = false
-}
-
 # Migrate state from the previous module-based layout. Resources not listed here
-# (staging/production branches, their protections, environments) are intentionally
-# absent from the config and will be destroyed on the next apply.
+# (staging/production branches, all branch protections, environments, devops/qa teams)
+# are intentionally absent from the config and will be destroyed on the next apply.
 moved {
   from = module.github_repo.github_repository.this
   to   = github_repository.this
@@ -74,9 +57,4 @@ moved {
 moved {
   from = module.github_repo.github_team_repository.this["development_brie"]
   to   = github_team_repository.development_brie
-}
-
-moved {
-  from = module.github_repo.github_branch_protection.main
-  to   = github_branch_protection.main
 }
